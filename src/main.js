85e19a9b7bdb1b45c8ebe67ebe14d751352693dd
Melvin Loos <melvin@looselycoupled.nl>
2018-07-07 16:31:41 +0200

    Added dynamic menu based on cockpit API

--- src/main.js
+++ src/main.js
@@ -4,14 +4,31 @@ import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
-import Home from './Pages/Home.vue'
+import Default from './Pages/Default.vue'
+import {HTTP} from './http-common.js';
 
 Vue.use(BootstrapVue)
 Vue.use(VueRouter)
 
-const routes = [
-  { path: '/', component: Home }
-]
+var errors = []
+var routes = []
+
+HTTP.get('collections/get/pages')
+.then(response => {
+  const pages = response.data.entries
+  for (var i = 0; i < pages.length; i++) {
+    var page = pages[i]
+    routes.push({
+      path: page.url,
+      component: Default,
+      props: { page: page },
+      title: page.title
+    })
+  }
+})
+.catch(e => {
+  errors.push(e)
+})
 
 const router = new VueRouter({
   routes // short for `routes: routes`
@@ -20,5 +37,8 @@ const router = new VueRouter({
 const app = new Vue({
   router: router,
   el: '#app',
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  data: {
+    errors: errors
+  }
+})
